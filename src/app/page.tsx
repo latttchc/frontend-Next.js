f4ec7c82bf486e28c8460a94eb1764807dd95489
latttchc <iam.yuk2882@gmail.com>
2025-04-28 14:33:29 +0900

    feat: 削除Mutationと成功時の再取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,37 @@
 'use client';
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import apiRouter from "@/api/router";
 
 
 export default function Home() {
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: ['getUsers'],
     queryFn: apiRouter.users.getUsers,
   });
 
-  console.log(data);
+  const deleteMutation = useMutation({
+    mutationFn: apiRouter.users.deleteUser,
+    onSuccess: () => {
+      refetch();
+    },
+  })
+
+
 
   return (
-    <main className="flex min-h-screen flex-col gap-[32px] justify-between items-center p-24">
-      <ul>
+    <main className="flex min-h-screen flex-col gap-4 items-center p-24">
+      <ul className="text-center w-96">
         {data?.map((user) => (
-          <li key={user.id}>
-            {user.name} - {user.email}
+          <li className="flex border-b border-gray-800 py-4 justify-between gap-2" key={user.id}>
+            <span className="text-white text-bold">{user.name}</span>
+            <span className="">{user.email}</span>
+            <button
+              className=""
+              onClick={() => deleteMutation.mutate(user)}
+            >Delete</button>
           </li>
         ))}
       </ul>
-    </main>
+    </main >
   );
 }
